b799fea199fb9d4d18877b9b735a3a8994fdb175
calishu <danny.kretschmann@proton.me>
2025-08-18 00:11:09 +0200

    feat: added master header file

---
+++ src/cstl.h
@@ -0,0 +1,19 @@
+/* cstl - A lightweight C99 header-only library focussing on
+ *        recreating the C++ STL, or at least its features.
+ *
+ * Copyright (c) 2025 calishu
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * This piece of software is licensed under the Apache-2.0 License,
+ * for more information check out the `LICENSE` file or visit following site:
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * GitHub Repository: https://github.com/calishu/cstl
+ * Documentation:     https://github.com/calishu/cstl/blob/main/docs/
+ * Ko-Fi (<3):        https://ko-fi.com/calishu
+ * */
+
+#ifndef CSTL_H
+#define CSTL_H
+
+#endif
